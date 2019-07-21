dec0dbda45dae52410a803cd317c8ff6a19902cb
phq <phq@p.com>
2019-07-21 22:41:04 +0800

    打包脚本可以不用写死插件名了

--- gz.bat
+++ gz.bat
@@ -1,9 +1,6 @@
-@echo off
-
-del *.tar
-del *.gz
-
-set packageDir=package
+::@echo off
+set workDir=%~dp0
+set packageDir=%workDir%package
 
 if exist %packageDir% (
 	rd %packageDir% /s/q
@@ -11,23 +8,26 @@ if exist %packageDir% (
 	md %packageDir%
 )
 
-call :myPackage ideaServer
-call :myPackage virtualHere
-del *.tar
-dir %packageDir%
+for /d %%i in (plugins/*) do (call :myPackage %%i)
+
+
+
 pause
 exit /b 0
-
 :myPackage
-	set workDir=%~dp0
 	set app=%1
 	echo app is : [%app%]
-	xcopy  %app% %packageDir%\%app%\ /se
-	xcopy  base %packageDir%\%app%\ /se
-	cd %workDir%\%packageDir%
-	%workDir%\7z a %app%.tar %app%
-	%workDir%\7z a -tgzip %app%.tar.gz %app%.tar
+	
+	xcopy  %workDir%\plugins\%app% %packageDir%\%app%\ /se
+	xcopy  %workDir%\base %packageDir%\%app%\ /se
+	echo uninstall %app%>>%packageDir%\%app%\uninstall.sh
+	
+	cd %packageDir%
+	%workDir%7z a %app%.tar %app%
+	%workDir%7z a %app%.tar %workDir%\%app%	
+	%workDir%7z a -tgzip %app%.tar.gz %app%.tar
 	del %app%.tar
+	
 	cd %workDir%
 goto:eof
 
